1d8906ad588471a68f1ee7484ca6d326b5ae433d
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-08-17 10:47:45 +0200

    Add project files.

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2243584#content
+ 
+ Method Overriding:
+- modifying the implementaton of an inherited method
+ 
+ */
+namespace MethodOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
